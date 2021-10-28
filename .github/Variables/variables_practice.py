690bac61e721feb4c63f3de465efd083b3120d6a
Jemari Davis <90348444+Vbandzgreens@users.noreply.github.com>
2021-10-28 12:04:43 -0400

    Create variables_practice.py

---
+++ .github/Variables/variables_practice.py
@@ -0,0 +1,11 @@
+# Variable Practice, Jemari Davis, 10/28/21 11:41, version 0.0
+
+# Variable Types -- Most Common
+
+my_float = 1.8977
+my_int = 689376888
+my_string = "I like dogs"
+
+print(my_float)
+print(my_int)
+print(my_string)
